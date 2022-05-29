aba220adda26e2b1e9e44d3cb8786f7ddbb74f6c
naara cataldo <naarabelencataldo@outlook.com>
2022-05-29 17:02:18 -0300

    =

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+
+function abrir(){
+document.getElementById('nav').style.display="inline-block";
+document.getElementById('menu-desplegable').style.display="inline-block";
+}
+function cerrar(){
+    document.getElementById('nav').style.display="none";
+document.getElementById('menu-desplegable').style.display="none";
+}
+
+
+
+
